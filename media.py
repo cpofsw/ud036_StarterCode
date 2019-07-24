a0050a45b09ab384cbc924cfc27534182034b469
Caio-Prado-Oliveira <cpo.31@outlook.com>
2019-07-24 13:55:50 -0300

    Modified media.py according to PEP8

--- media.py
+++ media.py
@@ -1,15 +1,16 @@
 import webbrowser
 
-#Define a class Movie e suas propriedades.
+# Define a class Movie e suas propriedades.
+
 
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):  # noqa
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    #Define metodo de chamada para mostrar os trailers.
-    
+    # Define metodo de chamada para mostrar os trailers.
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
